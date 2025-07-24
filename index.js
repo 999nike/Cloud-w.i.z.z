42195c4c3021c8d1e694871bfaf45623e8a84115
999nike <999nike@gmail.com>
2025-07-24 21:54:54 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  const msg = req.query.msg || "Hello from Cloud Wizz!";
-  res.send(`Wizz says: ${msg}`);
-});
-
-module.exports = app;
